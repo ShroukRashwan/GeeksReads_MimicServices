9325a4b942bc41e65221bd0fc885297918d9f40f
MahmoudMorsy <mahmoud_morsy@live.com>
2019-05-01 00:53:26 +0200

    Updated Signup.php mimic service to be exactly the same as latest deployed signup API with same inputs, returns and error codes

--- Morsy/Signup.php
+++ Morsy/Signup.php
@@ -1,19 +1,23 @@
 <?php
 
-$string = $_REQUEST["Json"];
-$json_in = json_decode($string, true);
+// To Read Data From Post Request Body like Backend
+$json_in =  json_decode(file_get_contents('php://input'), true);
+
 $json_out = new \stdClass();
 
 if ($json_in['UserEmail'] == "mahmoud_1@live.com")
 {
-    $json_out->ReturnMsg = "A verification email has been sent to mahmoud_1@live.com";
+	header("HTTP/1.1 200");
+    $json_out->ReturnMsg = "A verification email has been sent to mahmoud_1@live.com.";
 }
 else if ($json_in['UserEmail'] == "mahmoud_2@live.com")
 {
-    $json_out->ReturnMsg = "User already registered";
+	header("HTTP/1.1 400");
+    $json_out->ReturnMsg = "User already registered.";
 }
 else
 {
+	header("HTTP/1.1 400");
     $json_out->ReturnMsg = "An Error Occurred";
 }
 
